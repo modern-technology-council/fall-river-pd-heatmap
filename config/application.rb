ff7eff65b70dca8762c87d8e43bcc096e06592a3
Joshua Rumbut <joshua.rumbut@gmail.com>
2014-01-09 14:38:35 -0500

    silences useless i18n warning

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module FallRiverHeatmapApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # silence warnings because we don't do any I18n in this app right now.
+    config.i18n.enforce_available_locales = false
+
   end
 end
